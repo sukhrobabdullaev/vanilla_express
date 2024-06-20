e88217ece75936d11b09849a136e44784904d67e
sukhrobabdullaev <sukhrobjonabdullaev3365@gmail.com>
2024-06-20 14:39:49 +0500

    dynamic title hbs props

--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,14 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/login", (req, res) => {
-  res.render("login");
+  res.render("login", {
+    title: "Login",
+  });
 });
 
 router.get("/register", (req, res) => {
-  res.render("register");
+  res.render("register", {
+    title: "Register",
+  });
 });
 export default router;
