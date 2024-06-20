490901f51102d3a8b9c037daa9a5a576413e8fef
sukhrobabdullaev <sukhrobjonabdullaev3365@gmail.com>
2024-06-20 17:57:56 +0500

    model pass hashing and login API handler

---
+++ models/user.model.js
@@ -0,0 +1,11 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+const User = model("User", userSchema);
+export default User;
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
+import User from "../models/user.model.js";
 const router = Router();
+import bcrypt from "bcrypt";
 
+// RENDER AUTHS
 router.get("/login", (req, res) => {
   res.render("login", {
     title: "Login",
@@ -13,14 +16,40 @@ router.get("/register", (req, res) => {
   });
 });
 
-router.post("/login", (req, res) => {
-  console.log(req.body);
+// POST AUTHS
+router.post("/login", async (req, res) => {
+  const existUser = await User.findOne({ email: req.body.email });
+  if (!existUser) {
+    console.log("USER NOT FOUND");
+    return;
+  }
+  const isPassEqual = await bcrypt.compare(
+    req.body.password,
+    existUser.password
+  );
+  if (!isPassEqual) {
+    console.log("PASSWORD WRONG");
+    return;
+  }
+
+  console.log(existUser);
   res.redirect("/");
 });
 
-router.post("/register", (req, res) => {
-  console.log(req.body);
-  res.redirect("/");
+router.post("/register", async (req, res) => {
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  try {
+    const userData = {
+      ...req.body,
+      password: hashedPassword,
+    };
+    const user = await User.create(userData);
+    console.log(user);
+    res.status(201).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
 });
 
 export default router;
