77477e2e76ab943f25674d44d179b9ddaf01db3c
sukhrobabdullaev <sukhrobjonabdullaev3365@gmail.com>
2024-06-20 16:39:54 +0500

    mongodb

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import { create } from "express-handlebars";
 import productsRouter from "./routes/products.js";
 import authRouter from "./routes/auth.js";
+import mongoose from "mongoose";
+import "dotenv/config";
 
 const app = express();
 
@@ -13,14 +15,26 @@ const hbs = create({
 app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 app.set("views", "./views");
-app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static("public"));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(authRouter);
 app.use(productsRouter);
 
 const PORT = process.env.PORT || 8081;
-app.listen(PORT, () =>
-  console.log(`Listening on the port ${PORT}: http:localhost:${PORT}`)
-);
+
+async function fetchMongo() {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    app.listen(PORT, () =>
+      console.log(`Listening on the port ${PORT}: http:localhost:${PORT}`)
+    );
+    console.log(`MOGODB IS CONNECTED`);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+fetchMongo();
